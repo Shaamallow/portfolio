ce5d808e50eaee55e0157813cf7c0df728f82068
Shaamallow <eyal.benaroche@gmail.com>
2023-11-20 18:39:44 +0100

    add blog
    self Hosting services
    add small description to fill up the website

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -14,6 +14,65 @@
 			My Blog
 		</h1>
 
-		<p class="text-center m-5 italic">More infromation will be coming here soon enough</p>
+		<p class="text-center m-5 italic">
+			More infromation will be coming here soon enough, in the meanwhile...
+		</p>
+
+		<div class="divider m-10" />
+
+		<h2 class="font-display text-center text-3xl md:text-5xl font-bold mb-5">Self-Hosting</h2>
+
+		<p class="text-justify mb-5">
+			After a year at the Binet Réseau, I have come to a linking of being a system administrator and
+			enjoy the benefits of owning a server. Therefore, I've built my own server and I'm
+			self-hosting a bunch of services on top of my own website.
+		</p>
+
+		<h3 class="font-display text-center text-2xl md:text-4xl font-bold mb-5">Stack & Services</h3>
+
+		<div class="flex justify-center items-center my-3 gap-4 md:gap-10">
+			<a href="https://docs.docker.com/" target="_blank">
+				<img
+					src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/97_Docker_logo_logos-512.png"
+					alt="docker"
+					class="mt-3 w-20"
+				/>
+			</a>
+			<a href="https://gitlab.com" target="_blank">
+				<img
+					src="https://cdn.icon-icons.com/icons2/2415/PNG/512/gitlab_original_logo_icon_146503.png"
+					alt="gitlab"
+					class="mt-3 w-20"
+				/>
+			</a>
+			<a href="https://nextcloud.com" target="_blank">
+				<img
+					src="https://cdn.icon-icons.com/icons2/2699/PNG/512/nextcloud_logo_icon_168948.png"
+					alt="nextcloud"
+					class="mt-3 w-20"
+				/>
+			</a>
+			<a href="https://jellyfin.org" target="_blank">
+				<img src="/jellyfin.png" alt="jellyfin" class="mt-3 w-20" />
+			</a>
+
+			<a href="https://debian.org">
+				<img
+					src="https://cdn.iconscout.com/icon/free/png-256/free-debian-11-1175231.png"
+					alt="debian"
+					class="mt-3 w-20"
+				/>
+			</a>
+		</div>
+
+		<h3 class="flex items-center font-display text-2xl md:text-4xl font-bold mt-10">
+			<span>I use</span>
+			<img
+				alt="neovim"
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Neovim-mark.svg/1200px-Neovim-mark.svg.png"
+				class="mx-4 w-10"
+			/>
+			<span>btw</span>
+		</h3>
 	</div>
 </main>
