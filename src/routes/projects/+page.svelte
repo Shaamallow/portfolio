14324bd01422a282224e3811bd63e6836df626e0
Shaamallow <eyal.benaroche@gmail.com>
2023-11-20 18:46:42 +0100

    add contact link for linkedin
    fix fill color in svg

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
-
-	let currentColor = 'hsl(var(--bc))';
 </script>
 
 <svelte:head>
@@ -26,7 +24,7 @@
 					xmlns="http://www.w3.org/2000/svg"
 					x="0px"
 					y="0px"
-					fill={currentColor}
+					fill="hsl(var(--bc))"
 					width="100"
 					height="100"
 					viewBox="0 0 24 24"
